90ec981d1c14116b1185da519efe128d306ed5f9
Pavel Vasilev <pv9998286620@yandex.ru>
2021-12-13 19:27:08 +0300

    final changes v1.0

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ console.log(button);
 button.addEventListener("click", () => {
 	let word_1 = document.querySelector("#i-1").value;
 	let word_2 = document.querySelector("#i-2").value;
-    let outs = document.querySelectorAll('.info>p');
-    console.log(outs);
+	let outs = document.querySelectorAll(".info-item");
 
-	console.log(word_1);
-	console.log(word_2);
+	outs.forEach(item => item.classList.remove('active'));
+
+	if (word_1.length === 0 || word_2.length === 0) return;
 
 	let result =
-		word_1.length != 0 &&
 		word_1.length == word_2.length &&
 		word_1.split("").sort().join("").toLowerString == word_2.split("").sort().join("").toLowerString;
-    
-    if (result) outs[0].classList.add('active');
-    else outs[1].classList.add('active');
+
+	if (result) {
+		outs[0].classList.add("active");
+	} else {
+		outs[1].classList.add("active");
+	}
 });
-
-
